11acc0f44dfd3d4bdc812e076a449160502c8302
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2024-01-07 13:19:36 -0300

    login form

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#fffafc",
+          100: "#fff0f3",
+          200: "#fde2e7",
+          300: "#f5d4d9",
+          400: "#e9b5bd",
+          500: "#d98c9a",
+          600: "#c56f82",
+          700: "#b24c63",
+          800: "#8d2b45",
+          900: "#64222e",
+          950: "#3d1a1f",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
