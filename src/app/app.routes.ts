f54fc2ba28db612f547e640726049ccb5e79e679
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2024-01-09 19:14:18 -0300

    delete account form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { DeleteAccountComponent } from './delete-account/delete-account.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login-to-delete-account', component: LoginComponent },
+  { path: 'delete-account', component: DeleteAccountComponent },
 ];
---
+++ src/app/delete-account/delete-account.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-account.component.html',
+})
+export class DeleteAccountComponent {}
