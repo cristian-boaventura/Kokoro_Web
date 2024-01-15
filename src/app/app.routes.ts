825960ac74a5b7dc6161513d96163d9a6c59548e
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2024-01-15 19:29:37 -0300

    terms and conditions, privacy policies, and unknown routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { DeleteAccountComponent } from './delete-account/delete-account.component';
 import { authGuard, loginGuard } from './auth.guard';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
+import { PrivacyPoliciesComponent } from './privacy-policies/privacy-policies.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,4 +18,10 @@ export const routes: Routes = [
     component: DeleteAccountComponent,
     canActivate: [authGuard],
   },
+  { path: 'terms-and-conditions', component: TermsAndConditionsComponent },
+  {
+    path: 'privacy-policies',
+    component: PrivacyPoliciesComponent,
+  },
+  { path: '**', redirectTo: '' },
 ];
